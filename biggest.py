4f63e432b67c101b4186c5cb6688496d8cab9ccd
nimithaantany <41038431+nimithaantany@users.noreply.github.com>
2018-09-12 14:45:44 +0530

    Create biggest.py

---
+++ biggest.py
@@ -0,0 +1,9 @@
+a=int(input("enter the number"))
+b=int(input("enter the number"))
+c=int(input("enter the number"))
+if(a>b):
+ print("a is big")
+elif(c<b):
+ print("b is big")
+else:
+ print("c is big")
